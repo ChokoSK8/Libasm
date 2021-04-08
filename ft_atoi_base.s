2f5a7e162e1ce7e1696090d2678e870dd0195183
user42 <antoine.brunSK8@live.fr>
2021-04-08 14:55:08 +0200

    ll

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -8,28 +8,38 @@ ft_atoi_base:
 			call	ft_strlen
 			mov		r9, rax
 			dec		r9
-			mov		r10, rdi
+			pop		rdi
+			xor		rdi, rdi
 			mov		rdi, rsi
 			call	ft_strlen
+			jmp		end
 			mov		r11, rax
 			mov		rsi, rdi
-			mov		bl, BYTE [r10]
-			movzx	rdi, bl
+			xor		r13, r13
+			jmp		end
+
+get_n:
+			mov		dil, BYTE [r10]
+			cmp		r9, 0
+			je		end
 			call	get_index
+			cmp		r9, 1
+			je		end
 			mov		r8, rax
 			mov		r12, rsi
 			mov		rdi, r11
 			mov		rsi, r9
 			call	ft_power
+			mov		rsi, r12
 			mul		r8
-			mov		r13, rax
+			add		r13, rax
 			inc		r10
-			cmp		r10, 0
-			je		end
+			dec		r9
 			jmp		get_n
-
-get_n:
 			
 
-end:		mov	rax, r13
+end:		
+			movzx	rdi, bl
+			call	get_index
+			add		rax, r13
 			ret
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abrun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 10:41:17 by abrun             #+#    #+#             */
-/*   Updated: 2021/04/08 12:10:03 by abrun            ###   ########.fr       */
+/*   Updated: 2021/04/08 14:33:46 by abrun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 
 int		main(int ac, char **av)
 {
-	printf("Res : %d\n", ft_atoi_base("34e", "0123456789abcdef"));
+	printf("Res : %d\n", ft_atoi_base("32e", "0123456789abcdef"));
+	printf("Index : %d\n", get_index('f', "0123456789abcdef"));
 }
